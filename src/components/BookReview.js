99a529732fc38c34c63e1e334dc17debd4d51d32
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-09-08 10:10:55 +0300

    [update] add background image

--- src/components/BookReview.js
+++ src/components/BookReview.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 
 const BookReview = () => {
+  const[reviews, setReview] = useState([])
+  
+   useEffect(() => {
+      fetch("http://localhost:9292/reviews")
+      .then((response) => response.json())
+      .then((data) =>{
+        setReview(data)
+      })
+    },[]);
+  const showReview = reviews.map((review) => (
+    <ol key={review.id}>
+      <h1>Score:{review.score}</h1>
+      <p>comment:{review.comment}</p>
+    </ol>
+))  
+  
   return (
     <div>
-      <h1>Review here</h1>
+     {showReview}
     </div>
   )
 }
