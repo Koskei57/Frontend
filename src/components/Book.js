4d33fc696bca31488aa6b452ba62f2a119e893fa
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-09-09 22:54:08 +0300

    [update] add styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import NewBook from './NewBook';
 import DeleteBook from './DeleteBook';
+import EditBook from './EditBook';
 
 const Book = () => {
   
@@ -23,19 +24,25 @@ const [books, setBooks] = useState([]);
     const newBooks = books.filter((book) => book.id !== deletedBook.id )
     setBooks(newBooks)
   }
-
+  function handleEditChange(editedBook) {
+  
+  }
 
   return (
     <>
-      <div className='card'>
-        <ul className="book">
+      <div className='w-full min-h-screen flex justify-center items-center'>
+      <div className="w-100 p-2 bg-white rounded-x2">
+        <ul className="">
         {books.map((book) => (
           <DeleteBook id={book.id} key={book.id} book={book}  onDeleteBook={handleDelete} />
         ))}
       </ul>
-        <NewBook onAddBook={onAddBook} />
+          <NewBook onAddBook={onAddBook} />
+          <div className='edit'>
+            <EditBook onEditBook={handleEditChange} />
+          </div>
       </div>
-      
+      </div>
     </>
   )
 }
