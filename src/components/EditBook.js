0fa23a1e0a8065f15bdb85843ad37c19c86a3935
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-09-09 22:58:13 +0300

    Added tailwind configuration

---
+++ src/components/EditBook.js
@@ -0,0 +1,32 @@
+import React  from 'react'
+
+const EditBook = ({ book, onEditBook }) => {
+//  const{title,publisher,price} = book
+  
+  
+    function handleFormSubmit(e) {
+    e.preventDefault();
+
+    fetch(`http://localhost:9292/book/${book.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title:"", publisher:"", price:""})
+    })
+      .then((r) => r.json())
+      .then(() => onEditBook(book));
+  }
+
+    
+  return (
+    <div>
+      <form className="form" onSubmit={handleFormSubmit}>
+              <button className="btn" type='submit'>Edit</button>  
+    </form>
+    </div>
+  )
+}
+
+export default EditBook
+
